7b46dc892201fb7334922cf6678250d534134afe
Radwan Salem <85997938+radwansalem2030@users.noreply.github.com>
2021-12-19 08:00:25 +0200

    ok

---
+++ src/components/App.js
@@ -0,0 +1,42 @@
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { handleInitialData } from '../actions/shared';
+import { connect } from 'react-redux';
+import Dashboard from './Dashboard';
+import LoadingBar from 'react-redux-loading';
+import NewTweet from './NewTweet';
+import TweetPage from './TweetPage';
+import Nav from './Nav';
+class App extends Component {
+  componentDidMount() {
+    this.props.handleInitialData();
+  }
+  render() {
+    return (
+      <Router>
+        <Fragment>
+          <LoadingBar />
+          <div className="container">
+            <Nav />
+            {this.props.loading === true ? null : (
+              <div>
+                <Route path="/" exact component={Dashboard} />
+                <Route path="/tweet/:id" component={TweetPage} />
+                <Route path="/new" component={NewTweet} />
+              </div>
+            )}
+          </div>
+        </Fragment>
+      </Router>
+    );
+  }
+}
+
+
+function mapStateToProps({ authedUser }) {
+  return {
+    loading: authedUser === null
+  };
+}
+
+export default connect(mapStateToProps,{ handleInitialData })(App);
